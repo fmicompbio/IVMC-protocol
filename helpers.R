32a5bb8269a1fb13c756bc7d6c48147ce7f3e3f6
Charlotte Soneson <charlottesoneson@gmail.com>
2025-07-03 14:14:00 +0200

    Add params and helpers
    Co-authored-by: Hans-Rudolf Hotz <hrhotz@googlemail.com>
    Co-authored-by: Charlotte Soneson <charlottesoneson@gmail.com>

---
+++ helpers.R
@@ -0,0 +1,133 @@
+# ------------------------------------------------------------------------------
+# Helper functions
+# ------------------------------------------------------------------------------
+## Replace the get_legend function from cowplot temporarily,
+## since the cowplot one doesn't work with ggplot2 3.5.0
+## see https://github.com/wilkelab/cowplot/issues/202
+
+get_legend2 <- function(plot, legend = NULL) {
+    gt <- ggplotGrob(plot)
+
+    pattern <- "guide-box"
+    if (!is.null(legend)) {
+        pattern <- paste0(pattern, "-", legend)
+    }
+
+    indices <- grep(pattern, gt$layout$name)
+
+    not_empty <- !vapply(
+        gt$grobs[indices],
+        inherits, what = "zeroGrob",
+        FUN.VALUE = logical(1)
+    )
+    indices <- indices[not_empty]
+
+    if (length(indices) > 0) {
+        return(gt$grobs[[indices[1]]])
+    }
+    return(NULL)
+}
+
+.get_empty_label <- function(x) {
+    c("ciliated" = "", "non-ciliated" = "")
+}
+
+# Function to create normalized dot plots
+.makeDotPlot <- function(dat, markers, cluster_column, cluster_order,
+                         cluster_colors, transpose = TRUE,
+                         split_by_ciliated = FALSE, normalize = TRUE,
+                         subset_to_clusters = NULL,
+                         min_fraction = 0) {
+    # average expression
+    averages <- assay(sumCountsAcrossCells(
+        as.matrix(assay(dat, "logcounts")[markers, , drop = FALSE]),
+        dat[[cluster_column]],
+        average = TRUE, store.number = NULL)
+    )
+
+    # fraction of cells where gene is detected
+    ndetected <- assay(numDetectedAcrossCells(
+        as.matrix(assay(dat, "logcounts")[markers, , drop = FALSE]),
+        dat[[cluster_column]], average = TRUE)
+    )
+
+    stopifnot(rownames(averages) == rownames(ndetected),
+              colnames(averages) == colnames(ndetected))
+    if (min_fraction > 0) {
+        # keep only genes that are detected in at least a certain fraction in 
+        # at least one group
+        keep <- which(rowMaxs(ndetected) >= min_fraction)
+        averages <- averages[keep, , drop = FALSE]
+        ndetected <- ndetected[keep, , drop = FALSE]
+    }
+    plot.data <- data.frame(
+        Feature = factor(rep(rownames(averages), ncol(averages)),
+                         levels = rownames(averages)),
+        Group = rep(colnames(averages), each = nrow(averages)),
+        Average = as.numeric(averages),
+        Detected = as.numeric(ndetected)
+    ) |>
+        mutate(Group2 = ifelse(grepl("ciliated", Group), "ciliated", "non-ciliated")) |>
+        mutate(dummy = Group2)
+
+    if (normalize) {
+        # scale average expressions per gene
+        plot.data <- plot.data |>
+            group_by(Feature) |>
+            mutate(Average = (Average - min(Average)) / (max(Average) - min(Average))) |>
+            ungroup()
+    }
+
+    # subset to requested clusters
+    if (!is.null(subset_to_clusters)) {
+        plot.data <- plot.data |> 
+            filter(Group %in% subset_to_clusters)
+    }
+    
+    # reorder groups
+    plot.data <- plot.data |> 
+        mutate(Group = factor(Group, levels = rev(cluster_order))) |>
+        arrange(Group) |>
+        mutate(Group = as.character(Group)) |>
+        mutate(Group = paste("<span style = 'color: ",
+                             cluster_colors[Group],
+                             ";'>",
+                             Group,
+                             "</span>", sep = "")) |>
+        mutate(Group = factor(Group, levels = unique(Group)))
+
+    # plot
+    gg <- ggplot(plot.data) +
+        geom_point(aes(x = Group, y = Feature, size = Detected, col = Average)) +
+        scale_size(limits = c(0.2, max(plot.data$Detected))) +
+        scale_color_gradientn(colours = dotplot_colors(seq(0, 7.5, length.out = 100))) +
+        theme(panel.background = element_rect(fill = "white"),
+              panel.grid.major = element_line(linewidth = 0.5, colour = "grey80"),
+              panel.grid.minor = element_line(linewidth = 0.25, colour = "grey80"),
+              axis.title = element_blank(),
+              axis.ticks = element_blank())
+    if (transpose) {
+        gg <- gg +
+            coord_flip() +
+            theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 14),
+                  axis.text.y = element_markdown(size = 14))
+    } else {
+        gg <- gg +
+            theme(axis.text.y = element_text(size = 14),
+                  axis.text.x = element_markdown(angle = 90, vjust = 0.5, hjust = 1, size = 14))
+    }
+    if (split_by_ciliated) {
+        gg <- gg +
+            scale_x_discrete(expand = expansion(mult = c(0, 0), add = c(0.5, 0.5))) +
+            facet_nested(Group2 + dummy ~ ., scales = "free", space = "free",
+                         switch = "y", nest_line = element_line(linewidth = 1),
+                         solo_line = TRUE, resect = unit(2, "mm"), render_empty = TRUE,
+                         labeller = labeller(dummy = .get_empty_label,
+                                             Group2 = "label_value")) +
+            theme(strip.placement = "outside",
+                  strip.background = element_rect(color = "transparent", fill = "transparent"),
+                  strip.text = element_text(size = 14),
+                  panel.spacing = unit(0, "lines"))
+    }
+    gg
+}
---
+++ params.R
@@ -0,0 +1,308 @@
+# ------------------------------------------------------------------------------
+# bulk RNAseq timecourse
+# ------------------------------------------------------------------------------
+bulk_timepoint_colors <- c(
+    "tp0" = "#e6e6e6",
+    "Pre-diff" = "#e6e6e6",
+    "tp1" = "#5e17d1",
+    "P4-diff" = "#5e17d1",
+    "tp2" = "#989692",
+    "Horm\nWithdr 24h" = "#989692",
+    "Horm Withdr 24h" = "#989692",
+    "H. W.\n24h" = "#989692",
+    "H. W. 24h" = "#989692",   
+    "tp3" = "#989692",
+    "Horm\nWithdr 48h" = "#989692",
+    "Horm Withdr 48h" = "#989692",
+    "H. W.\n48h" = "#989692",
+    "H. W. 48h" = "#989692",   
+    "tp4" = "#cb3b63",
+    "Repair\n24h" = "#cb3b63",
+    "Repair 24h" = "#cb3b63",
+    "Post-breakdown\n24h" = "#cb3b63",
+    "Post-breakdown 24h" = "#cb3b63",
+    "P. B. 24h" = "#cb3b63",
+    "tp5" = "#debaf5",
+    "E2-diff\n24h" = "#debaf5",
+    "E2-diff 24h" = "#debaf5",
+    "tp6" = "#BE9FD2",
+    "E2-diff\n48h" = "#BE9FD2",
+    "E2-diff 48h" = "#BE9FD2"
+)
+
+bulk_timepoint_map_sl <- c(
+    tp0 = "Pre-diff", tp1 = "P4-diff", tp2 = "Horm Withdr 24h",
+    tp3 = "Horm Withdr 48h", tp4 = "Post-breakdown 24h", tp5 = "E2-diff 24h",
+    tp6 = "E2-diff 48h"
+)
+bulk_timepoint_map_mll <- c(
+    tp0 = "Pre-diff", tp1 = "P4-diff", tp2 = "Horm\nWithdr 24h",
+    tp3 = "Horm\nWithdr 48h", tp4 = "Post-breakdown\n24h", tp5 = "E2-diff\n24h",
+    tp6 = "E2-diff\n48h"
+)
+bulk_timepoint_map_mlm <- c(
+    tp0 = "Pre-diff", tp1 = "P4-diff", tp2 = "Horm Withdr\n24h",
+    tp3 = "Horm Withdr\n48h", tp4 = "Post-breakdown\n24h", tp5 = "E2-diff\n24h",
+    tp6 = "E2-diff\n48h"
+)
+bulk_timepoint_map_mls <- c(
+    tp0 = "Pre-diff", tp1 = "P4-diff", tp2 = "H. W.\n24h",
+    tp3 = "H. W.\n48h", tp4 = "P. B.\n24h", tp5 = "E2-diff\n24h",
+    tp6 = "E2-diff\n48h"
+)
+
+breakdown_colors <- c(
+    NB = "#7f7f7f", 
+    B = "#A30B2E"
+)
+hormones_colors <- c(
+    NH = "#CCCCC4",
+    H = "#00C5CD"
+)
+donor_colors <- c(
+    B032 = "#670E10",
+    B044 = "#63B7B7",
+    B050 = "#FF9966",
+    B066 = "#006400",
+    B078 = "#4F86F7",
+    B080 = "#a6761d"
+)
+
+# for volcano plots
+down <- "Down"
+notsig <- "Not Sig"
+up <- "Up"
+volcano_colors <- setNames(c("cornflowerblue", "grey", "firebrick"), c(down, notsig, up))
+
+# ------------------------------------------------------------------------------
+# scRNAseq timecourse
+# ------------------------------------------------------------------------------
+sc_timepoint_colors <- c(
+    "Horm Withdr 48h" = "grey40",
+    "Post-breakdown 12h" = "#f3a9c0",
+    "Post-breakdown 24h" = "#e27897",
+    "Post-breakdown 48h" = "#cb3b63",
+    "E2-diff 24h" = "#debaf5",
+    "E2-diff 48h" = "#BE9FD2"
+)
+sc_timepoint_order <- names(sc_timepoint_colors)
+
+sc_leiden_res0.125_annot_colors <- c(
+    "ciliated" = "royalblue",
+    "ciliated immunomodulatory" = "tan1",
+    "ciliated wound responsive 24h" = "turquoise4",
+    "ciliated wound responsive 12h" = "turquoise",
+    "ciliated stress responsive" = "navy",
+    "ciliated cycling" = "olivedrab2",
+    "pre-ciliated" = "lightskyblue",
+    "secretory late" = "firebrick",
+    "secretory early" = "firebrick1",
+    "hormone responsive" = "orchid3",
+    "immunomodulatory" = "#FF6347",
+    "OxPhos high" = "coral4",
+    "wound responsive 24h" = "goldenrod1",
+    "wound responsive 12h" = "navajowhite",
+    "stress responsive" = "grey20",
+    "cycling" = "forestgreen"
+)
+
+sc_leiden_res0.125_annot_order <- names(sc_leiden_res0.125_annot_colors)
+
+# marker genes (for dot plot)
+sc_timecourse_markers <- c( 
+    "MKI67", "PCNA", "BIRC5", "MCM7", "HSPA1A", "DNAJB1", "GADD45A", "JUN", 
+    "EGR1", "ARC", "CCN1", "CCN2", "RRS1", "LYAR", "F3", "TNFRSF12A", "DKK1", 
+    "SFN", "FGF2", "IFRD1", "WWC1", "SEMA7A", "MALT1", "ADAMTS9", "LDLR", 
+    "ZNRF3", "SH3RF2", "WEE1", "AMOTL1", "PDGFA", "EDN2", "CXCL8", "CXCL3",
+    "PLAU", "PLAUR", "WNT7A", "CD47", "MMP7", "IL32", "KRT17", "TNF", "TNFAIP2", 
+    "IL1R1", "CXCL10", "VTCN1", "IRAK2", "CSF1", "LIF", "SAA1", "SAA2", 
+    "ESR1", "PGR", "SOX17", "LGR5", "LGR4", "COL1A2", "ALDH1A1", "CDH2", 
+    "SCGB1D2", "SCGB2A1", "PTGS2", "MT1F", "CALB1", "PAX8", "SERPINA5", "C3", 
+    "MUC5B", "PAEP", "PAX2", "EGLN3", "HILPDA", "CCNO", "E2F7", "MCIDAS", 
+    "CDC20B", "MAD2L1", "CRELD2", "DNAJB11", "NEK2", "PIFO", "FOXJ1", 
+    "RSPH1", "TPPP3") 
+
+# ------------------------------------------------------------------------------
+# scRNAseq tissue atlas (epithelial)
+# ------------------------------------------------------------------------------
+tissue_leiden_res0.4_annot_colors <- c(
+    "ciliated" = "royalblue",
+    "pre-ciliated" = "lightskyblue",
+    "secretory late" = "#3A1260",
+    "secretory mid" = "#7D4DBA",
+    "secretory early" = "#C8AEE2",
+    "hormone responsive late" = "hotpink4",
+    "hormone responsive early" = "hotpink2",
+    "luminal late" = "darkorange",
+    "luminal early" = "gold1",
+    "KRT5+" = "firebrick",
+    "MUC5B+" = "salmon",
+    "transcriptionally active" = "turquoise4",
+    "remodeling" = "#4CAF50",
+    "cycling" = "mediumaquamarine"
+)
+tissue_leiden_res0.4_annot_order <- names(tissue_leiden_res0.4_annot_colors)
+
+tissue_markers <- c(
+    "PAX8", "EPCAM", "FOXA2", "MKI67", "STMN1", "PCNA", "VIM", "NABP1",
+    "TEAD1", "NKTR", "PNN", "MACF1", "PHLDA1", "KRT5", "KRT17", "CLU",
+    "LY6E", "C3", "PIGR", "MUC5B", "LCN2", "KRT23", "BPIFB1", "CFTR",
+    "SAA1", "SAA2", "PLAUR", "PLAU", "IL32", "WNT7A", "CSF1", "CD47",
+    "TNF", "MMP7", "ITGA2", "TNFRSF12A", "CLDN22", "SULT1E1", "LEFTY1",
+    "PTGS1", "IL6", "LGR5", "PAX2", "MT1H", "ENPP3", "ESR1", "PGR", "AR",
+    "COL1A2", "MMP26", "SOX17", "SCGB1D2", "SCGB2A1", "SCGB2A2", "IL6ST",
+    "VEGFA", "VCAN", "DYNLT3", "MAOA", "PPIB", "GPX3", "PAEP", "TSPAN8",
+    "NUPR1", "SOD3", "FGF7", "IGFBP7", "CCNO", "CDC20B", "MAD2L1", "NEK2",
+    "PIFO", "FOXJ1", "RSPH1", "TPPP3")
+
+tissue_goterms <- list(
+    "luminal early" = c("epithelium development", "cell-cell junction organization",
+                        "retina vasculature morphogenesis in camera-type eye",
+                        "regulation of systemic arterial blood pressure by endothelin",
+                        "epithelial cell differentiation",
+                        "positive regulation of cell growth",
+                        "response to wounding", "cell communication",
+                        "cell migration", "regulation of protein localization",
+                        "skin development", "tissue development",
+                        "response to stimulus", "cell adhesion"),
+    "luminal late" = c("gastrulation", "stem cell proliferation",
+                       "fat cell differentiation",
+                       "negative regulation of response to stimulus",
+                       "p38MAPK cascade",
+                       "positive regulation of intracellular mRNA localization",
+                       "negative regulation of biological process",
+                       "negative regulation of cell communication",
+                       "response to growth factor",
+                       "negative regulation of cellular process",
+                       "negative regulation of signal transduction",
+                       "regulation of programmed cell death",
+                       "negative regulation of macromolecule metabolic process",
+                       "response to corticotropin-releasing hormone"),
+    "transcriptionally active" = c("nucleic acid metabolic process", 
+                                   "RNA splicing", "gene expression",
+                                   "cell-cell adhesion mediated by cadherin",
+                                   "RNA metabolic process",
+                                   "peptidyl-serine phosphorylation",
+                                   "RNA biosynthetic process", 
+                                   "nucleic acid biosynthetic process", 
+                                   "negative regulation of hippo signaling", 
+                                   "hippo signaling")
+)
+
+# ------------------------------------------------------------------------------
+# endometrial atlas (all lineages)
+# ------------------------------------------------------------------------------
+tissue_all_lineages_celltype_cols <- c(
+    Venous = "firebrick", 
+    Arterial = "firebrick1",
+    Lymphatic = "firebrick2",
+    Mast_cell = "cyan2",
+    pDC = "cyan2",
+    cDC1 = "cyan4",
+    cDC2 = "cyan4",
+    Monocyte = "steelblue",
+    eM1 = "steelblue1",
+    eM2 = "steelblue3",
+    ILC3 = "royalblue2",
+    Peripheral_lymphocyte = "hotpink4",
+    T_cell_CD4 = "plum1",
+    T_cell_CD8 = "plum4",
+    T_cell_cycling = "plum",
+    T_Reg = "pink2",
+    uNK1_cycling = "violetred4",
+    uNK1 = "violetred4",
+    uNK2 = "violetred2",
+    uNK3 = "violetred4",
+    B_cell = "red2",
+    Plasma_B_cell = "red2",
+    Red_blood_cell = "red2",
+    uSMCs = "orchid",
+    ePV = "bisque",
+    mPV = "bisque3",
+    eStromal = "seagreen1",
+    dStromal = "seagreen4",
+    Fibroblast_basalis = "green4",
+    tissue_leiden_res0.4_annot_colors,
+    HOXA13 = "grey90",
+    sHormones = "grey70",
+    dHormones = "grey70"
+)
+tissue_all_lineages_celltype_lineage <- c(
+    Venous = "Endothelial", 
+    Arterial = "Endothelial",
+    Lymphatic = "Endothelial",
+    Mast_cell = "Immune",
+    pDC = "Immune",
+    cDC1 = "Immune",
+    cDC2 = "Immune",
+    Monocyte = "Immune",
+    eM1 = "Immune",
+    eM2 = "Immune",
+    ILC3 = "Immune",
+    Peripheral_lymphocyte = "Immune",
+    T_cell_CD4 = "Immune",
+    T_cell_CD8 = "Immune",
+    T_cell_cycling = "Immune",
+    T_Reg = "Immune",
+    uNK1_cycling = "Immune",
+    uNK1 = "Immune",
+    uNK2 = "Immune",
+    uNK3 = "Immune",
+    B_cell = "Immune",
+    Plasma_B_cell = "Immune",
+    Red_blood_cell = "Immune",
+    uSMCs = "Mesenchymal",
+    ePV = "Mesenchymal",
+    mPV = "Mesenchymal",
+    eStromal = "Mesenchymal",
+    dStromal = "Mesenchymal",
+    Fibroblast_basalis = "Mesenchymal",
+    setNames(rep("Epithelial", length(tissue_leiden_res0.4_annot_colors)),
+             names(tissue_leiden_res0.4_annot_colors)),
+    HOXA13 = "Epithelial",
+    sHormones = "Epithelial",
+    dHormones = "Epithelial"
+)
+
+# ------------------------------------------------------------------------------
+# other
+# ------------------------------------------------------------------------------
+cc_colors <- c(
+    "S" = "gold",
+    "G1" = "grey80",
+    "G2M" = "brown"
+)
+
+dotplot_colors <- circlize::colorRamp2(
+    breaks = c(0, 1.875, 3.75, 5.625, 7.5),
+    colors = c("#d9d9d9", "#f4a582", "#d6604d", "#8b0000", "#5c0000")
+)
+
+stage_colors <- c(
+    "early-secretory" = "#F4EBFA",
+    "early-mid-secretory" = "#C8AEE2",
+    "proliferative" = "hotpink2",
+    "mid-secretory" = "#7D4DBA",
+    "late-secretory" = "#3A1260",
+    "menstrual" = "#A30B2E"
+)
+
+dataset_colors <- c(
+    "GarciaAlonso" = "mediumaquamarine",
+    "Shih" = "darkblue",
+    "Wang" = "goldenrod1",
+    "Huang" = "firebrick"
+)
+
+# ------------------------------------------------------------------------------
+# Marker gene signatures (for plots)
+# ------------------------------------------------------------------------------
+luminal_early_genes <- c(
+    "PLAU", "IL32", "WNT7A", "CSF1", "CD47", "TNF", "MMP7", "ITGA2",
+    "TNFRSF12A", "NOTCH3", "EGFR", "ITGA3", "CRABP2", "TIMP2", "CLDN7",
+    "CAMTA1", "FOSL1", "EDN1")
+luminal_late_genes <- c(
+    "CLDN22", "SULT1E1", "LEFTY1", "PTGS1", "IL6", "LGR5", "PAX2",
+    "MT1H", "ENPP3", "GATA2", "LATS2", "LGR4", "SOCS3", "FOS", "FOSB",
+    "NR4A3", "SCGB1D2", "SERPINA5", "ID1", "KLF10")
+
